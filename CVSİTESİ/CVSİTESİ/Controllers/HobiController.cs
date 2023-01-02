17332839bacef83ec1c5c5cb95bbd8ded3e818f5
Sümeyye <52049640+smyyyaydn@users.noreply.github.com>
2023-01-03 00:44:03 +0300

    yenilendi

---
+++ CVSİTESİ/CVSİTESİ/Controllers/HobiController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CVSİTESİ.Models.Entity;
+using CVSİTESİ.Repositories;
+
+namespace CVSİTESİ.Controllers
+{
+    public class HobiController : Controller
+    {
+        // GET: Hobi
+        GenericRepository<TblHobilerim> repo = new GenericRepository<TblHobilerim>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var hobiler = repo.List();
+            return View(hobiler);
+        }
+        [HttpPost]
+        public ActionResult Index(TblHobilerim p)
+        {
+            //TblHobilerim t = new TblHobilerim();
+            var t = repo.Find(x => x.ID == 1);
+            t.Açıklama1 = p.Açıklama1;
+            repo.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
