540557dfa27ad65802481c0929a606975cc4b8db
Sümeyye <52049640+smyyyaydn@users.noreply.github.com>
2023-01-09 00:33:34 +0300

    commit

---
+++ CVSİTESİ/CVSİTESİ/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using CVSİTESİ.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CVSİTESİ.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(TblAdmin p)
+        {
+            dbcvsitesiEntities db = new dbcvsitesiEntities();
+            var bilgi = db.TblAdmin.FirstOrDefault(x=>x.KullanıcıAdı==p.KullanıcıAdı && x.Şifre==p.Şifre );
+            if(bilgi!=null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgi.KullanıcıAdı, false);
+                Session["KullanıcıAdı"]= bilgi.KullanıcıAdı.ToString();
+                return RedirectToAction("Index", "Deneyim");
+            }
+
+            else
+            {
+                return RedirectToAction("Index", "Login");
+            }
+        }
+    }
+}
---
+++ CVSİTESİ/CVSİTESİ/Controllers/SosyalMedyaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CVSİTESİ.Models.Entity;
+using CVSİTESİ.Repositories;
+
+namespace CVSİTESİ.Controllers
+{
+    public class SosyalMedyaController : Controller
+    {
+        // GET: SosyalMedya
+        GenericRepository<TblSosyalMedya> repo = new GenericRepository<TblSosyalMedya>();
+        
+
+        public ActionResult Index()
+        {
+            var veriler = repo.List();
+            return View(veriler);
+        }
+        [HttpGet]
+        public ActionResult Ekle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Ekle(TblSosyalMedya P)
+        {
+            repo.TAdd(P);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
